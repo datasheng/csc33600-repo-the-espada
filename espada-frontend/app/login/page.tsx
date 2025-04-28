b19753e00b28a7df8363c33ec2ad352c45c48fcd
Debasree Sen <102424463+wallaweebs@users.noreply.github.com>
2025-04-28 15:52:24 -0400

    updated signup login

--- espada-frontend/app/login/page.tsx
+++ espada-frontend/app/login/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import Form from 'next/form'
 import Header from '../components/Header';
 import Footer from '../components/Footer'
 
@@ -14,14 +13,14 @@ export default function Login() {
 
                         <div className=" relative bg-black/70 text-gray-100 px-8 py-6 rounded-lg border border-solid border-yellow-400 border-10">
                             <h1 className="font-bold py-2 text-center mb-2 text-2xl text-yellow-400">Login</h1>
-                            <Form action="/login">
+                            <form action="/login">
                                 <div className="mb-4">
                                     <label className="block mb-2">Email</label>
-                                    <input type="text" className="border border-solid border-gray-400 w-full"></input>
+                                    <input type="email" className="border border-solid border-grey w-full" required></input>
                                 </div>
                                 <div className="mb-4">
                                     <label className="block mb-2">Password</label>
-                                    <input type="password" className="border border-solid border-gray-400 w-full"></input>
+                                    <input type="password" className="border border-solid border-grey w-full" required></input>
                                 </div>
                                 <div className="flex justify-center">
                                     <button className="text-white rounded-lg border border-solid border-yellow-400  flex items-center 
@@ -29,7 +28,7 @@ export default function Login() {
                                     drop-shadow-sm"
                                     type="submit">Log In</button>
                                 </div>
-                            </Form>
+                            </form>
                             <div className="text-center">
                             <p className="text-sm m-2">
                             Don’t have an account yet? <a href="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
--- espada-frontend/app/signup/page.tsx
+++ espada-frontend/app/signup/page.tsx
@@ -1,9 +1,11 @@
 "use client"
-import Form from 'next/form'
 import Header from '../components/Header';
 import Footer from '../components/Footer'
+import { useState } from 'react';
 
 export default function Signup() {
+    const [newAccount, setNewAccount] = useState()
+
     return (
         <>
             <Header />
@@ -14,22 +16,22 @@ export default function Signup() {
                         <div className="absolute inset-0 bg-white/70 z-0"></div>
                         <div className=" relative bg-black/70 text-gray-100 border-yellow-400 border-10 px-14 py-8 rounded-lg border border-solid  border-10">
                             <h1 className="font-bold py-2 text-center text-2xl mb-2 text-yellow-400">Sign Up</h1>
-                            <Form action="/login">
+                            <form action="/login" method="POST">
                                 <div className="mb-4">
                                     <label className="block mb-2">First Name</label>
-                                    <input type="text" className="border border-solid border-grey w-full"></input>
+                                    <input type="text" className="border border-solid border-grey w-full" required></input>
                                 </div>
                                 <div className="mb-4">
                                     <label className="block mb-2">Last Name</label>
-                                    <input type="text" className="border border-solid border-grey w-full"></input>
+                                    <input type="text" className="border border-solid border-grey w-full" required></input>
                                 </div>
                                 <div className="mb-4">
                                     <label className="block mb-2">Email</label>
-                                    <input type="text" className="border border-solid border-grey w-full"></input>
+                                    <input type="email" className="border border-solid border-grey w-full" required></input>
                                 </div>
                                 <div className="mb-4">
                                     <label className="block mb-2">Password</label>
-                                    <input type="password" className="border border-solid border-grey w-full"></input>
+                                    <input type="password" className="border border-solid border-grey w-full" required></input>
                                 </div>
                                 <div className="flex justify-center">
                                     <button className="text-white rounded-lg border border-solid border-yellow-400  flex items-center 
@@ -37,7 +39,7 @@ export default function Signup() {
                                     drop-shadow-sm"
                                     type="submit">Sign Up</button>
                                 </div>
-                            </Form>
+                            </form>
                             <div className="text-center">
                             <p className="text-sm m-2">
                             Have an account yet already? <a href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login</a>
