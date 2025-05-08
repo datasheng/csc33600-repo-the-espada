b2e41410532ba5734f0ce6d73f34d424661bbf5c
sdemetriusjenkins <sdemetriusjenkins@gmail.com>
2025-05-08 18:58:32 -0400

    Fixed Links

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -162,8 +162,8 @@ const Header: React.FC = () => {
           )}
         </div>
         <Link href="/map" className={styles.navLink}>Map</Link>
-        <Link href="/AboutUs" className={styles.navLink}>About Us</Link>
-        <Link href="/ContactUs" className={styles.navLink}>Contact</Link>
+        <Link href="/about-us" className={styles.navLink}>About Us</Link>
+        <Link href="/contact-us" className={styles.navLink}>Contact</Link>
         <Link href="/login" className={styles.authButton}>Login</Link>
         <Link href="/signup" className={styles.authButton}>Sign Up</Link>
       </nav>
